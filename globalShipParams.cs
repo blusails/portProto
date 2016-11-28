628334af7d3392aba7ec4a10681e46441f0da619
blusails <caplinger.james@gmail.com>
2016-11-28 00:14:34 -0500

    Add files via upload
    fixed path follow inconsistencies, added variable ship speeds and other global ship parameters

---
+++ globalShipParams.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class globalShipParams : MonoBehaviour {
+    public float indicatorLightFrequency;
+    public float minPathSpacing;  // number of moveSteps adjacent movePath values must be apart
+    public float tolAngle;
+    public float maxMoveStep;
+    public float minMoveStep;
+
+}
--- shipController.cs
+++ shipController.cs
@@ -7,25 +7,34 @@ public class shipController : MonoBehaviour {
     public Vector3[] movePath;
     private bool moving = false;
     private int pathIndex=0;
-    //private Rigidbody shipRB;
+    private Rigidbody shipRB;
     private Transform shipTransform;
     public Vector3 advanceVec;
     public Vector3 previousAdvanceVec;
+    private float indicatorLightFrequency = 5.0f;
+    private float minPathSpacing = 5.0f;  // number of moveSteps adjacent movePath values must be apart
+    private float tolAngle=30.0f;
+    private float moveStep;
+    private float maxMoveStep;
+    private float minMoveStep;
 
+    public float speed;  // boat speed, value between 0 and 1;
     // 
     //Use this for initialization
     void Start () {
         indicatorLight = GetComponent<Light>();
-        //shipRB = GetComponent<Rigidbody>();
+        shipRB = GetComponent<Rigidbody>();
         shipTransform = GetComponent<Transform>();
-        
+        updateGlobalVars();
+
+
     }
 	
 	// Update is called once per frame
 	void FixedUpdate () {
         if (isSelected)
         {
-            indicatorLight.intensity = Mathf.Abs(10.0f * Mathf.Sin(Time.realtimeSinceStartup*5.0f));
+            indicatorLight.intensity = Mathf.Abs(10.0f * Mathf.Sin(Time.realtimeSinceStartup* indicatorLightFrequency)); // oscillate light frequency to indication selection
         }
 
         if (moving)
@@ -36,35 +45,51 @@ public class shipController : MonoBehaviour {
 	
 	}
 
-    public void onSelect()
+    public void onSelect()   
     {
         isSelected = true;
-        indicatorLight.intensity = 30.0f;
+        indicatorLight.intensity = 30.0f;  // turn on indicatorLight
         
     }
 
     public void onDeselect()
     {
         isSelected = false;
-        indicatorLight.intensity = 0.0f;
+        indicatorLight.intensity = 0.0f;  // turn off indicatorLight
     }
 
+    void updateGlobalVars()
+    {
+        GameObject masterController = GameObject.Find("masterController");
+        globalShipParams gSP = masterController.GetComponent<globalShipParams>();
+        indicatorLightFrequency = gSP.indicatorLightFrequency;
+        minPathSpacing = gSP.minPathSpacing;  // number of moveSteps adjacent movePath values must be apart
+        tolAngle = gSP.tolAngle;
+        maxMoveStep = gSP.maxMoveStep;
+        minMoveStep = gSP.minMoveStep;
+
+    }
     public void move(Vector3[] path)
     {
         //print("moving");
-        movePath = conditionPath(path);
+        moveStep = speed * maxMoveStep;
+        if (moveStep < minMoveStep) moveStep = minMoveStep;
+        if (moveStep > maxMoveStep) moveStep = maxMoveStep;
+
+        movePath = conditionPath(path);  // remove redundant points and ensure minimum point spacing of 20.0*moveStep
        // print(movePath.Length);
         moving = true;
 
-        advanceVec = movePath[pathIndex] - shipTransform.position;
-        advanceVec.Normalize();
+        //advanceVec = movePath[pathIndex] - shipTransform.position; // get first advanceVec
+        // advanceVec.Normalize();
+
         
-        tolAngle = 15;
+        
     }
 
 
-    public float moveStep;
-    private float tolAngle;
+    
+
 
     void advancePosition()
     {
@@ -72,22 +97,28 @@ public class shipController : MonoBehaviour {
 
 
        // print(Vector3.Angle(advanceVec, movePath[pathIndex] - shipTransform.position));
-        if (Vector3.Angle(advanceVec, movePath[pathIndex] - shipTransform.position) > tolAngle)
-        {
-            tolAngle = 15;
-            pathIndex++;
-            advanceVec = movePath[pathIndex] - shipTransform.position;
-            advanceVec.Normalize();
+        //if (Vector3.Angle(advanceVec, movePath[pathIndex] - shipTransform.position) > tolAngle)
+        //{
+            
+        //    pathIndex++;
 
-        }
-        if (pathIndex > movePath.Length-2)
+
+   
+        if (pathIndex > movePath.Length-1)
         {
             moving = false;
+            pathIndex = 0;
+            return;
         }
-        
+
+        advanceVec = movePath[pathIndex] - shipTransform.position;
+        advanceVec.Normalize();
 
         shipTransform.position= shipTransform.position + advanceVec * moveStep;
-        //ipRB.AddForce(advanceVec * moveStep);
+        
+        pathIndex++;
+
+        //shipRB.AddForce(advanceVec * moveStep);
         
     }
 
@@ -110,12 +141,20 @@ public class shipController : MonoBehaviour {
     private Vector3[] conditionPath(Vector3[] inArray)  // assumes adjacency
     {
         ArrayList tempVec = new ArrayList();
-        tempVec.Add(inArray[0]);
-        for (int i = 1; i < inArray.Length; i++)
+        Vector3 prevPoint = shipTransform.position;
+        tempVec.Add(shipTransform.position);
+        for (int i = 1; i +1< inArray.Length; i++)
         {
-            if (Vector3.Distance(inArray[i],inArray[i - 1])>moveStep*20.0f)
+            if (Vector3.Distance(inArray[i],prevPoint)>moveStep)  // interp between 
             {
-                tempVec.Add(inArray[i]);
+                prevPoint = Vector3.Lerp(prevPoint, inArray[i], moveStep/Vector3.Distance(prevPoint, inArray[i]));
+                tempVec.Add(prevPoint);
+                i--;
+            }
+            else
+            {
+                prevPoint = Vector3.Lerp(inArray[i], inArray[i + 1], moveStep / Vector3.Distance(inArray[i], inArray[i + 1]));
+                tempVec.Add(prevPoint);
             }
         }
 
